b3cdf316ddc8119bd5211afd24fc6b18c451bbb6
0war357 <bart89761@gmail.com>
2026-07-02 17:11:49 -0600

    fixed: cambios no tan  importantes

--- src/components/Usuarios/SupplierTable.vue
+++ src/components/Usuarios/SupplierTable.vue
@@ -1,7 +1,4 @@
 <template>
-
-
-
   <div class="bg-[#eef2e9] min-h-screen p-8 text-[#1a2e1f] font-['Inter',sans-serif]">
     <div class="flex flex-col mb-8 gap-4">
       <div class="flex justify-between items-center w-full">
@@ -204,3 +201,4 @@ const toggleEstado = async (proveedor) => {
   background-color: #eef2e9 !important;
 }
 </style>
+ 
